ba76a75a0f6aefa0ba112889e71d2349c06c3fcb
Cristiella <judithmukami1998@gmail.com>
2022-09-08 14:19:34 -0700

    quote redo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints input
- * Return: 1 if success
+ * main - input print
+ *
+ * Return: 1
  */
 int main(void)
 {
-	write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
 	return (1);
 }
-
