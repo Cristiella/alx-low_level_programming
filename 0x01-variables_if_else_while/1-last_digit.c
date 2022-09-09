a49dde4eae79f783a9421dfb6a831c3d0b946069
Cristiella <judithmukami1998@gmail.com>
2022-09-09 05:22:13 -0700

    last digit reredo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,34 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - last digit greater or less than 5
- * 
- * Return: Return 0
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
 	int n;
-	int last;
+	char last[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-
-	if (last == 0)
+	printf("%s %d is %d and is ", last, n, n % 10);
+	if (n % 10 > 5)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last);
+		printf("greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("greater than 5\n");
 	}
 	else
 	{
-		if (last > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, last);
-		}
-		else if (last < 6)
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-		}
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
