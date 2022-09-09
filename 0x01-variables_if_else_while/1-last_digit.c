33e7a4abdbd5f9cb9a639633c294f8c2af6a38c7
Cristiella <judithmukami1998@gmail.com>
2022-09-09 04:30:20 -0700

    ladt digit greater ir less than 5 redo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry
+ * main - the last digit if greater or less than 5
  *
- *Return: Always 0
+ * Return: 0 
  */
-
-int main(void)
+int main (void)
 {
 	int n;
+	char last[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	{
+		printf("greater than 5\n");
+	}
 	else if (n % 10 == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	{
+		printf("0\n");
+	}
 	else
-		
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-
+	{
+		printf("less than 6 and not 0\n");
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,5 @@ program will assign a random number to the variable n each time it is executed
 1-last_digit.c
 program will assign a random number to the variable n each time it is executed2-print_alphabet.c
 program that prints the alphabet in lowercase, followed by a new line
+3-print_alphabets.c
+program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
